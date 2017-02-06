0df0a02cc717f098e13dee8e6cd7ab059bcb2151
Ryan S. Brown <sb@ryansb.com>
2017-02-06 14:49:19 -0500

    Add function to create a new Service Catalog artifact
    The product in the catalog will have many artifacts based on the same
    project, so this also includes the object version in the template URL if
    it's available. That way old versions will be launchable forever since
    both code and template are versioned.

--- crop/utils.py
+++ crop/utils.py
@@ -2,7 +2,12 @@
 # Author: Ryan Scott Brown <sb@ryansb.com>
 # License: Apache v2.0
 
+import random
+import string
+
 import boto3
+
+from crop.logging import log
 
 def get_product(name=None, product_id=None):
     service = boto3.client('servicecatalog')
@@ -15,3 +20,36 @@ def get_product(name=None, product_id=None):
         product_id = product['ProductId']
 
     return service.describe_product(Id=product_id)['ProductViewSummary']
+
+def build_template_url(asset_bucket, template_key, version_id=None):
+    s3 = boto3.client('s3')
+    # get S3 regional bucket URL and build URL for template
+    template_url = '{}/{}/{}'.format(s3.meta.endpoint_url, asset_bucket, template_key)
+    if version_id is not None:
+        template_url += '?versionId={}'.format(version_id)
+    return template_url
+
+
+def update_product_artifact(product_id, version, template_url):
+    service = boto3.client('servicecatalog')
+
+    token = generate_idempotency_token()
+
+    log.info('provisioning_artifact.create.start', token=token, product=product_id, object_url=template_url)
+
+    artifact = service.create_provisioning_artifact(
+        ProductId=product_id,
+        Parameters={
+            'Name': version,
+            'Description': 'Deploy artifact by CROP',
+            'Info': {
+                'LoadTemplateFromURL': template_url,
+            },
+            'Type': 'CLOUD_FORMATION_TEMPLATE',
+        },
+        IdempotencyToken=token,
+    )
+    log.debug('provisioning_artifact.create.success', artifact=artifact)
+
+def generate_idempotency_token():
+    return ''.join(random.sample(string.ascii_letters*16, 16))
